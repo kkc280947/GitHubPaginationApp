4c0b7b3261edc6cba266d02c20cc148a27444aa5
kkc472809 <krishnachouhan414@gmail.com>
2020-03-09 12:18:50 +0530

    comment added in Network util

--- app/src/main/java/com/example/githubuserapp/utils/NetworkUtils.kt
+++ app/src/main/java/com/example/githubuserapp/utils/NetworkUtils.kt
@@ -3,6 +3,12 @@ package com.example.githubuserapp.utils
 const val BASE_URL = "https://api.github.com/"
 
 object Network {
+
+    /**
+     * Checking for internet connection without passing context (Not using Connectivity manager
+     * which needs context to check network state). Here we will get internet state by pinging at
+     * google.com. If Ping is successful return true else false
+     * */
     fun internetIsConnected(): Boolean {
         return try {
             val command = "ping -c 1 google.com"
